ec18921f718658ba56c1162c2185a5e5197b5be7
Agnesagbo <agnesagbo06@gmail.com>
2022-09-09 01:50:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        puts("\"programming is like building a multilingual puzzle");
+        return (0);	
